6545db998467d53850d65565bd11afd25137a645
streafy <streafyhs@gmail.com>
2026-03-03 23:38:04 +0700

    move files to ru.nsu package

---
+++ src/main/kotlin/ru/nsu/Codec.kt
@@ -0,0 +1,25 @@
+package ru.nsu
+
+import com.fasterxml.jackson.databind.JsonNode
+import java.lang.reflect.Type
+import java.nio.file.Path
+import kotlin.reflect.KClass
+
+interface Codec {
+
+    fun parse(json: String): JsonNode
+
+    fun parse(file: Path): JsonNode
+
+    fun write(node: JsonNode): String
+
+    fun writePretty(node: JsonNode): String
+
+    fun writeToFile(node: JsonNode, file: Path)
+
+    fun toJsonNode(value: Any?): JsonNode
+
+    fun <T : Any> decodeToClass(node: JsonNode, clazz: KClass<T>): T
+
+    fun decode(node: JsonNode, type: Type): Any?
+}
